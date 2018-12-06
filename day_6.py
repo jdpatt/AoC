78ed2d5fb1eff8fdb36a5a3a8d4f0d9857fc19ec
David Patterson <dpatter877@gmail.com>
2018-12-06 14:06:54 -0500

    Commit current working copy

--- day_6.py
+++ day_6.py
@@ -1,6 +1,18 @@
 """Advent of Code 2018 Day 6"""
 from common import get_puzzle_input
 
+
+def manhattan_distance(point_1, point_2):
+    """Return the Manhattan distance between two Cartesian coordinates."""
+    return abs(point_1[0] - point_2[0]) + abs(point_1[1] - point_2[1])
+
+
 if __name__ == "__main__":
-    COORDINATES = [(int(x[0]), int(x[1])) for x in (x.split(",") for x in get_puzzle_input("input6.txt"))]
-    print(COORDINATES)
+    COORDINATES = [
+        (int(x[0]), int(x[1]))
+        for x in (x.split(",") for x in get_puzzle_input("input6.txt"))
+    ]
+
+
+def test_manhattan_distance():
+    assert manhattan_distance((1, 1), (2, 2)) == 2
