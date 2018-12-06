c587bcf76bf1aa988861297d9c3b01ed86834ec4
David Patterson <dpatter877@gmail.com>
2018-12-05 22:05:42 -0500

    Add input and completed part 1.

---
+++ day_5.py
@@ -0,0 +1,40 @@
+"""Advent of Code 2018 Day 5"""
+
+
+def is_reactive(unit_a, unit_b):
+    """If the units are reactive return true."""
+    return unit_a == unit_b.swapcase()
+
+
+def reduce_polymer(polymer):
+    """Remove all reactive elements."""
+    reduced = []
+    for unit in polymer:
+        if reduced and is_reactive(unit, reduced[-1]):
+            reduced.pop()
+        else:
+            reduced.append(unit)
+    return "".join(reduced)
+
+
+if __name__ == "__main__":
+    with open("input5.txt", "r") as input_txt:
+        POLYMER = input_txt.read().strip()
+    print(f"Original Length: {len(POLYMER)}")
+    print(f"Reduced Length: {len(reduce_polymer(POLYMER))}")
+
+
+def test_is_reactive():
+    """Verify that swapcase works for all cases."""
+    assert is_reactive("A", "a")
+    assert is_reactive("a", "A")
+    assert not is_reactive("A", "A")
+    assert not is_reactive("a", "a")
+    assert not is_reactive("A", "B")
+
+
+def test_reduce_polymer():
+    """Verify that we actually reduce the string."""
+    assert reduce_polymer("aa") == "aa"
+    assert reduce_polymer("aA") == ""
+    assert reduce_polymer("aAa") == "a"
