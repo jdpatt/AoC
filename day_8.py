680a55db39178e773a6a63aa58e9ef2691e8cb75
David Patterson <dpatter877@gmail.com>
2018-12-08 21:32:46 -0500

    Part 2

--- day_8.py
+++ day_8.py
@@ -2,29 +2,43 @@
 from common import get_puzzle_input
 
 
-class Node(object):
+class Node:
     """Define what a node in the tree looks like."""
 
-    def __init__(self, parent, meta):
+    # pylint: disable=R0903
+
+    def __init__(self, parent):
         super(Node, self).__init__()
         self.parent = parent
-        self.meta = meta
+        self.meta = []
+        self.child_values = []
+        self.value = 0
 
 
 def generate_nodes(tree, data, parent):
     """Walk through the tree, recursively generating nodes"""
-    node = Node(parent, [])
+    node = Node(parent)
     num_of_children = next(data)
     num_of_metadata = next(data)
-    for child in range(num_of_children):
-        tree, data = generate_nodes(tree, data, node)
-    for meta in range(num_of_metadata):
+    for _ in range(num_of_children):
+        tree, data, val = generate_nodes(tree, data, node)
+        node.child_values.append(val)
+    for _ in range(num_of_metadata):
         node.meta.append(next(data))
+    if num_of_children == 0:
+        node.value = sum(node.meta)
+    else:
+        for val in node.meta:
+            try:
+                node.value += node.child_values[val - 1]
+            except IndexError:
+                pass
     tree.append(node)
-    return tree, data
+    return tree, data, node.value
 
 
-if __name__ == '__main__':
-    puzzle = [int(x) for x in get_puzzle_input("input8.txt")[0].split(" ")]
-    tree, _ = generate_nodes([], iter(puzzle), "Root")
-    print(f"Metadata sum: {sum([sum(node.meta) for node in tree])}")
+if __name__ == "__main__":
+    PUZZLE = [int(x) for x in get_puzzle_input("input8.txt")[0].split(" ")]
+    TREE, _, ROOT_VALUE = generate_nodes([], iter(PUZZLE), "Root")
+    print(f"Metadata sum: {sum([sum(node.meta) for node in TREE])}")
+    print(f"Root Value: {ROOT_VALUE}")
