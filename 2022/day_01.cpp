f7a9ace8664a272b832bf2c4776c338fb1f563a3
David Patterson <david.patterson@plexus.com>
2023-03-20 14:35:17 -0400

    feat: day 1 in cpp in prep for software role

---
+++ 2022/day_01.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    vector<int> elves;
+    int current_calories = 0;
+
+    ifstream infile("day_01_input.txt");
+    std::string line_in_file;
+    while (std::getline(infile, line_in_file))
+    {
+        if (line_in_file == "")
+        {
+            elves.push_back(current_calories);
+            current_calories = 0;
+        }
+        else
+        {
+
+            current_calories += std::stoi(line_in_file);
+        }
+    }
+
+    std::sort(elves.begin(), elves.end());
+    cout << "Part 1: Elf with the most calories: " << elves.back() << endl;
+
+    // Let it infer the op and just sum ints.
+    cout << "Part 2: Top Three Elves: " << std::accumulate(elves.end() - 3, elves.end(), 0) << endl;
+    return 0;
+}
